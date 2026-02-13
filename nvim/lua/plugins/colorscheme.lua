f895194d65f7b00078c4723ed0ec4a4697929670
duchiru <ngvit31082007@gmail.com>
2026-02-13 17:02:18 +0700

    feat: NeoVim OneDark color scheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			local onedark = require("onedark")
+			onedark.setup({ style = 'darker' })
+			onedark.load()
+		end
+	}
+}
