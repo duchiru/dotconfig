44306b3bf85698f025773f0c9604442b3454a5fc
duchiru <ngvit31082007@gmail.com>
2026-02-14 08:53:09 +0700

    feat: separated keybinds.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
--- IMPORTANT: Load options before bootstraping lazy.nvim
+-- IMPORTANT: Load every other config before bootstraping lazy.nvim
 require("config.options")
+require("config.keybinds")
 require("config.lazy")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,9 @@
+-- Misc
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+
+-- Indentation
 vim.opt.expandtab = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
