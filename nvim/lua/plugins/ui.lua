5688d54471868a59e23faf26e441a7d92d298cea
duchiru <ngvit31082007@gmail.com>
2026-02-15 17:18:24 +0700

    chore: modify noice.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,4 +1,38 @@
 return {
+	-- Better UI for some Neovim components
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			cmdline = {
+				view = "cmdline",
+				format = {
+					search_down = {
+						view = "cmdline",
+					},
+					search_up = {
+						view = "cmdline",
+					},
+				},
+			},
+			presets = {
+				long_message_to_split = true, -- long messages will be sent to a split
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+	},
+
 	-- Status line
 	{
 		"nvim-lualine/lualine.nvim",
