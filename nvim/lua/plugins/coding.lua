1c244d3f8be08a9f6c8e298d9fc974e2ead75c46
duchiru <ngvit31082007@gmail.com>
2026-02-16 20:11:38 +0700

    feat: popular code snippets

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,56 @@
+return {
+	-- Code completion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "VeryLazy",
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					-- { name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+		end,
+	},
+
+	-- Snippet system: LuaSnip
+	{
+		"saadparwaiz1/cmp_luasnip",
+		event = "VeryLazy",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+		},
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+		event = "VeryLazy",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+}
