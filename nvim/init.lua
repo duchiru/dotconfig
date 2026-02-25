daf54d8cef11ae5c91fce0231bb018d247734b30
duchiru <ngvit31082007@gmail.com>
2026-02-25 07:34:08 +0700

    feat: try lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,1 @@
--- IMPORTANT: Load every other config before bootstraping lazy.nvim
-require("config.options")
-require("config.keybinds")
 require("config.lazy")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,20 +1,7 @@
--- Line numbers: Show line numbers, highlight active line and relative numbers
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.relativenumber = true
-
--- File explorer: disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Indentation
 vim.opt.expandtab = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-
--- Diagnostic
-vim.diagnostic.enable(true)
-vim.diagnostic.config({ virtual_text = true })
 
 -- Swap files
 vim.opt.swapfile = false
